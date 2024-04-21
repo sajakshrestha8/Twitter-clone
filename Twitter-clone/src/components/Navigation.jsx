76f9111813d3a3c09fd0cdf765aad67dddaff1e7
sajakshrestha8 <shresthasajak123@gmail.com>
2024-04-21 07:54:32 +0545

    update

---
+++ Twitter-clone/src/components/Navigation.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Navigation extends React.Component{
+    render(){
+        return <>
+            <nav>
+                <ul>
+                    <li>Sports News</li>
+                    <li>International News</li>
+                    <li>National News</li>
+                    <li>Business News</li>
+                </ul>
+            </nav>
+        </>
+    }
+}
+
+export default Navigation;
